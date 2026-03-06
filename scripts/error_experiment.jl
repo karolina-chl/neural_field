afc75a0e80bb863ae0032b1989bee4845e9b79e9
karolina-chl <karolina.chlopicka@student.uva.nl>
2026-03-06 23:11:47 +0100

    mesh size experiment improved

--- scripts/error_experiment.jl
+++ scripts/error_experiment.jl
@@ -46,39 +46,48 @@ make_params(datafile_name) = (
     ),
     save_data = true,
     datafile_name = datafile_name
-)    
+)
 
-mesh_size_testset = (7,6,5)
+function integral_mesh(G_params)
+    ### Create a mesh
+    mesh_size = G_params.Ω.Ω_args.mesh_size
+    R = G_params.Ω.Ω_args.R
+    build_Ω = G_params.Ω.build_Ω
+
+    mesh = GT.with_gmsh(gmsh -> build_Ω(gmsh, mesh_size, R))
+    Ω = GT.interior(mesh)
+
+    ### Finite element interpolation
+    interpolation_degree = G_params.other.interpolation_degree
+    V = GT.lagrange_space(Ω,interpolation_degree)
+
+    ### Numerical integration 
+    integration_degree = G_params.other.integration_degree
+    dΩ = GT.quadrature(Ω,integration_degree)
+    return V, dΩ
+end
+
+mesh_size_testset = (7)
+results = Dict()
 
 for size in mesh_size_testset
     G_params = make_G_params(size)
     params = make_params("data_$size")
     main(G_params, S_params, params)
+
+    V, dΩ = integral_mesh(G_params)
+    data = JSON.parsefile("data/exp_raw/data_$size")
+    u_final_state = Float64.(data.u[1])
+
+    uh = GT.solution_field(V,u_final_state)
+    integral = GT.∫(x -> abs(uh(x)),dΩ) |> sum 
+    results["mesh_size: $size"] = integral
 end 
 
-### Create a mesh
-G_params = make_G_params(6) # must be adjutsed each time 
-mesh_size = G_params.Ω.Ω_args.mesh_size
-R = G_params.Ω.Ω_args.R
-build_Ω = G_params.Ω.build_Ω
 
-axis = (aspect = Makie.DataAspect(),)
-colormap=:viridis
-mesh = GT.with_gmsh(gmsh -> build_Ω(gmsh, mesh_size, R))
-Ω = GT.interior(mesh)
+# save the file with final results 
+    open("data/exp_raw/final-results", "w") do io
+        JSON.json(io, results)
+    end
 
-### Finite element interpolation
-interpolation_degree = G_params.other.interpolation_degree
-V = GT.lagrange_space(Ω,interpolation_degree)
-node_x = GT.node_coordinates(V)
 
-### Numerical integration 
-integration_degree = G_params.other.integration_degree
-dΩ = GT.quadrature(Ω,integration_degree)
-
-# read the file 
-data = JSON.parsefile("data/exp_raw/data_6") # must be adjusted 
-u_final_state = Float64.(data.u[1])
-
-uh = GT.solution_field(V,u_final_state)
-integral = GT.∫(x -> abs(uh(x)),dΩ) |> sum 
