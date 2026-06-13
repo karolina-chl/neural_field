fc2e52c06e5c7ee7c2d716c507556c986ca0f809
karolina-chl <karolina.chlopicka@student.uva.nl>
2026-06-13 16:13:38 +0200

    added/modified data processing and plotting functions for efficiency and strong scaling

--- src/data_processing.jl
+++ src/data_processing.jl
@@ -15,12 +15,12 @@ function get_max_rhs_time_over_repetitions(num_proc, num_reps, nx, ny, num_layer
         summed = sum.(rhs_times[num])
         push!(summed_arr, summed)
     end 
-    max_arr = []
+    slowest_worker = []
     for num in 1:num_reps
         max_time = maximum(summed_arr[rank][num] for rank in 1:num_proc)
-        push!(max_arr, max_time)
+        push!(slowest_worker, max_time)
     end 
-    return max_arr
+    return slowest_worker
 end  
 
 function get_strong_scaling_data(proc_list, num_reps, nx, ny, num_layers)
@@ -30,12 +30,21 @@ function get_strong_scaling_data(proc_list, num_reps, nx, ny, num_layers)
         push!(best_time_arr, minimum(max_arr))
     end     
     return best_time_arr
+end 
+
+function get_efficiency_data(proc_list, t1, num_reps, nx, ny, num_layers)
+    efficiency = []
+    for proc in proc_list
+        max_arr = get_max_rhs_time_over_repetitions(proc, num_reps, nx, ny, num_layers)
+        tp = minimum(max_arr)
+        eff = t1/(proc*tp)
+        push!(efficiency, eff)
+    end 
+    return efficiency     
 end     
 
-proc_list = [2,4,8,16,32,64]
-nx = 30
-ny = 30 
-num_reps = 10
-num_layers = 2
-best_time_arr = get_strong_scaling_data(proc_list, num_reps, nx, ny, num_layers)
-print(best_time_arr)
+function get_t1_time(nx, ny, num_layers)
+    data_t1 = JSON.parsefile("data/exp_raw/mpi_exp/results_nx$(nx)ny$(ny)np1num_layers$(num_layers).json")
+    t1 = minimum(sum.(data_t1[1]["rhs"]))
+    return t1       
+end 
