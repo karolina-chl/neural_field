08e441562a9892ad1226962ce39b20375f357f4c
karolina-chl <karolina.chlopicka@student.uva.nl>
2026-06-14 00:02:36 +0200

    changed job scheduling scripts + modified memory allocation in parallel_delayed

--- scripts/run_mpi_layers.sh
+++ scripts/run_mpi_layers.sh
@@ -5,7 +5,7 @@ export PATH="/var/scratch/tln716/julia/julia-1.12.4/bin:$PATH"
 export JULIA_DEPOT_PATH="/var/scratch/tln716/julia_depot"
 mkdir -p data/exp_raw/mpi_exp
 
-layer_values_arr=(2 3 4 5 6 7 8)
+layer_values_arr=(2 4 8)
 proc_arr=(1 2 4 8 16 32 64)
 
 for layer in ${layer_values_arr[@]}
