142dbc8a02934d927385b3b7545cf216de91c731
karolina-chl <karolina.chlopicka@student.uva.nl>
2026-07-24 12:08:03 +0200

    script restructure

--- scripts/cluster_scripts/das5/run_increasing_layer_das5.sh
+++ scripts/cluster_scripts/das5/run_increasing_layer_das5.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export PATH="/var/scratch/tln716/julia/julia-1.12.4/bin:$PATH"
-export JULIA_DEPOT_PATH="/var/scratch/tln716/julia_depot"
+source das5/modules.sh
 mkdir -p data/exp_raw
 
 layer_values_arr=(20)
